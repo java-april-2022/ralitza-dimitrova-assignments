94dcb4a098633175bd936256d9895b64197a4641
Ralitza Dimitrova <ralitzaidimitrova@gmail.com>
2022-04-10 12:35:35 -0500

    First Java Program

---
+++ Week1/Extra Practice/First Java Program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Ralitza Dimitrova");
+        System.out.println("I am 26 years old");
+        System.out.println("My hometown is Chicago, Illinois");
+    }
+}
